942a6b1b7db483fc29fa3bed6b723b1c7f116d73
Mdsakib999 <sakibabrar999@gmail.com>
2023-05-19 14:59:52 +0600

    some change on roboBg

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,121 +4,148 @@ import { Link } from "react-router-dom";
 const Gallery = () => {
   return (
     <div>
-      <p className="font-bold text-4xl my-16 text-center">
+      <p className="font-bold text-4xl mt-16 text-center">
         Our Unique Collection's
+      </p>
+      <p className="text-center text-slate-500 lg:w-[60%] mx-auto mt-4 mb-16 p-5 lg:p-0">
+        Introducing our exclusive product collection, where innovation meets
+        elegance, creating an extraordinary tapestry of style and functionality.
       </p>
       <div className="w-[95%] mx-auto grid lg:grid-cols-3 gap-y-8">
         <div className="card card-compact lg:w-80 bg-base-100 shadow-xl">
           <figure>
-            <img className="h-[300px] w-[100%]" src="https://i.ibb.co/8gd4zK0/carRobo3.jpg" alt="Shoes" />
+            <img
+              className="h-[300px] w-[100%]"
+              src="https://i.ibb.co/8gd4zK0/carRobo3.jpg"
+              alt="Shoes"
+            />
           </figure>
           <div className="card-body">
             <h2 className="card-title">R8 Monster</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
-            
               <Link to="/allToys">
-              <button
-                className="px-4 py-2 rounded-md
+                <button
+                  className="px-4 py-2 rounded-md
           text-white font-semibold text-base bg-gradient-to-r from-cyan-400 to-blue-500 hover:from-blue-500 hover:to-cyan-400 ..."
-              >
-                Buy Now
-              </button>
+                >
+                  Buy Now
+                </button>
               </Link>
             </div>
           </div>
         </div>
         <div className="card card-compact lg:w-80 bg-base-100 shadow-xl">
           <figure>
-            <img className="h-[300px] w-[100%]" src="https://i.ibb.co/XLwMdxR/fighter-Robo2.jpg" alt="Shoes" />
+            <img
+              className="h-[300px] w-[100%]"
+              src="https://i.ibb.co/XLwMdxR/fighter-Robo2.jpg"
+              alt="Shoes"
+            />
           </figure>
           <div className="card-body">
             <h2 className="card-title">Warrior Gang</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
-            <Link to="/allToys">
-              <button
-                className="px-4 py-2 rounded-md
+              <Link to="/allToys">
+                <button
+                  className="px-4 py-2 rounded-md
           text-white font-semibold text-base bg-gradient-to-r from-cyan-400 to-blue-500 hover:from-blue-500 hover:to-cyan-400 ..."
-              >
-                Buy Now
-              </button>
+                >
+                  Buy Now
+                </button>
               </Link>
             </div>
           </div>
         </div>
         <div className="card card-compact lg:w-80 bg-base-100 shadow-xl">
           <figure>
-            <img className="h-[300px] w-[100%]" src="https://i.ibb.co/5R2jRDj/animal-Robo2.webp" alt="Shoes" />
+            <img
+              className="h-[300px] w-[100%]"
+              src="https://i.ibb.co/5R2jRDj/animal-Robo2.webp"
+              alt="Shoes"
+            />
           </figure>
           <div className="card-body">
             <h2 className="card-title">Danger Daino!</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
-            <Link to="/allToys">
-              <button
-                className="px-4 py-2 rounded-md
+              <Link to="/allToys">
+                <button
+                  className="px-4 py-2 rounded-md
           text-white font-semibold text-base bg-gradient-to-r from-cyan-400 to-blue-500 hover:from-blue-500 hover:to-cyan-400 ..."
-              >
-                Buy Now
-              </button>
+                >
+                  Buy Now
+                </button>
               </Link>
             </div>
           </div>
         </div>
         <div className="card card-compact lg:w-80 bg-base-100 shadow-xl">
           <figure>
-            <img className="h-[300px] w-[100%]" src="https://i.ibb.co/RYdcbhM/713-EYg68j3-L-AC-UL600-SR600-600.jpg" alt="Shoes" />
+            <img
+              className="h-[300px] w-[100%]"
+              src="https://i.ibb.co/RYdcbhM/713-EYg68j3-L-AC-UL600-SR600-600.jpg"
+              alt="Shoes"
+            />
           </figure>
           <div className="card-body">
             <h2 className="card-title">Robo Tommy</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
-            <Link to="/allToys">
-              <button
-                className="px-4 py-2 rounded-md
+              <Link to="/allToys">
+                <button
+                  className="px-4 py-2 rounded-md
           text-white font-semibold text-base bg-gradient-to-r from-cyan-400 to-blue-500 hover:from-blue-500 hover:to-cyan-400 ..."
-              >
-                Buy Now
-              </button>
+                >
+                  Buy Now
+                </button>
               </Link>
             </div>
           </div>
         </div>
         <div className="card card-compact lg:w-80 bg-base-100 shadow-xl">
           <figure>
-            <img className="h-[300px] w-[100%]" src="https://i.ibb.co/VDZmDH4/fighter-Robo1.jpg" alt="Shoes" />
+            <img
+              className="h-[300px] w-[100%]"
+              src="https://i.ibb.co/VDZmDH4/fighter-Robo1.jpg"
+              alt="Shoes"
+            />
           </figure>
           <div className="card-body">
             <h2 className="card-title">Robo Fighter</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
-            <Link to="/allToys">
-              <button
-                className="px-4 py-2 rounded-md
+              <Link to="/allToys">
+                <button
+                  className="px-4 py-2 rounded-md
           text-white font-semibold text-base bg-gradient-to-r from-cyan-400 to-blue-500 hover:from-blue-500 hover:to-cyan-400 ..."
-              >
-                Buy Now
-              </button>
+                >
+                  Buy Now
+                </button>
               </Link>
             </div>
           </div>
         </div>
         <div className="card card-compact lg:w-80 bg-base-100 shadow-xl">
           <figure>
-            <img className="h-[300px] w-[100%] p-2" src="https://i.ibb.co/MDNdTNV/rovoCar.webp" alt="Robo Car" />
+            <img
+              className="h-[300px] w-[100%] p-2"
+              src="https://i.ibb.co/MDNdTNV/rovoCar.webp"
+              alt="Robo Car"
+            />
           </figure>
           <div className="card-body">
             <h2 className="card-title">Car Monster</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div className="card-actions justify-end">
-            <Link to="/allToys">
-              <button
-                className="px-4 py-2 rounded-md
+              <Link to="/allToys">
+                <button
+                  className="px-4 py-2 rounded-md
           text-white font-semibold text-base bg-gradient-to-r from-cyan-400 to-blue-500 hover:from-blue-500 hover:to-cyan-400 ..."
-              >
-                Buy Now
-              </button>
+                >
+                  Buy Now
+                </button>
               </Link>
             </div>
           </div>
--- src/components/RoboBg.jsx
+++ src/components/RoboBg.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const RoboBg = () => {
+  
   return (
     <div className="relative rounded-xl">
       <img
@@ -11,7 +12,7 @@ const RoboBg = () => {
       />
 
       <div className="absolute top-0 lg:left-16 rounded-xl bg-gradient-to-r from-cyan-300 to-[rgba(50, 5, 5, 0)] w-[70%] h-full">
-        <p className="lg:pl-32 pl-20 pt-20 lg:pt-20 lg:font-bold text-4xl lg:w-[60%] text-black">
+        <p className="lg:pl-32 pl-20 pt-20 lg:pt-20 lg:font-bold text-4xl lg:w-[60%] text-white font-mono">
           Unleash endless adventures with our incredible line of robot toys.
         </p>
         <Link className="pl-32 " to="/allToys">
